a834cced304ae2bd0fae3146601d8e04b0c596ad
mohamed muflahi <muflahimohamed88@gmail.com>
2022-02-02 13:19:13 -0500

    commit tree b10a7be9dc1ce94616e83c0da0dbfe5389ee6158

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+const findMatching = (arr,string) => {
+    const result = arr.filter(element => element.toLowerCase() === string.toLowerCase());
+    return result;
+}
+const fuzzyMatch = (arr,string) => {
+    const result = arr.filter(element => element.toLowerCase().startsWith(string.toLowerCase()));
+    return result;
+}
+const matchName = (arr, string) => {
+    const result = arr.filter(element => element.name.toLowerCase() === string.toLowerCase());
+    return result;
+}
